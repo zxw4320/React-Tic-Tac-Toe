f72f9cc1870d1f9952253af0edc7937bd5db9455
zwigent <zxw4320@rit.edu>
2020-04-16 10:08:19 -0400

    starting react tutorial

---
+++ src/components/square.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Square extends React.Component {
+    render() {
+      return (
+        <button className="square" onClick={() => alert('click')}>
+          {this.props.value}
+        </button>
+      );
+    }
+}
+
+export default Square;
